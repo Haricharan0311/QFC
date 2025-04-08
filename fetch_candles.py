aaa7a68092d455e97cbf027ba7707e62f913242e
Shubham Vijay Tidke <42945527+ShubhamTidke@users.noreply.github.com>
2025-04-08 23:18:32 +0000

    ShubhamTidke | Add next

--- fetch_candles.py
+++ fetch_candles.py
@@ -1,27 +1,24 @@
 from hyperliquid.info import Info
 from hyperliquid.utils import constants
 from datetime import datetime, timezone, timedelta
-import json
 
 # Initialize the Info class
 info = Info(constants.MAINNET_API_URL, skip_ws=True)
 
 # Define parameters
-symbols = {"ETH", "BTC"}
+# symbols = {"ETH", "BTC", "VINE"}
+symbols = {"VINE"}
 interval = "15m"
+candles = {}
 
 # Set the time range for the candlestick data
 # For example, the last 24 hours
 end_time = int(datetime.now(timezone.utc).timestamp() * 1000)  # Current time in milliseconds
-candles = []
 for symbol in symbols:
     start_time = end_time - (60 * 60 * 1000 * 1265)  # 24 hours ago in milliseconds
 
     # Fetch candlestick data
-    candles = info.candles_snapshot(name=symbol, interval=interval, startTime=start_time, endTime=end_time)
-    print(len(candles))
-    # end_time = start_time
-
+    candles[symbol] = info.candles_snapshot(name=symbol, interval=interval, startTime=start_time, endTime=end_time)
+    
 # Process the data as needed
-print(len(candles))
-# print(json.dumps(candles, indent=2))
+print(candles["ETH"])
