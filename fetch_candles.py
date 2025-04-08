ef6d9b710e1f32aaabc84d108e4b1829721026e5
Shubham Vijay Tidke <42945527+ShubhamTidke@users.noreply.github.com>
2025-04-08 22:25:56 +0000

    ShubhamTidke | Add trade_data class

--- fetch_candles.py
+++ fetch_candles.py
@@ -7,16 +7,21 @@ import json
 info = Info(constants.MAINNET_API_URL, skip_ws=True)
 
 # Define parameters
-symbol = "ETH"
-interval = "1m"
+symbols = {"ETH", "BTC"}
+interval = "15m"
 
 # Set the time range for the candlestick data
 # For example, the last 24 hours
 end_time = int(datetime.now(timezone.utc).timestamp() * 1000)  # Current time in milliseconds
-start_time = end_time - (24 * 60 * 60 * 1000)  # 24 hours ago in milliseconds
+candles = []
+for symbol in symbols:
+    start_time = end_time - (60 * 60 * 1000 * 1265)  # 24 hours ago in milliseconds
 
-# Fetch candlestick data
-candles = info.candles_snapshot(name=symbol, interval=interval, startTime=start_time, endTime=end_time)
+    # Fetch candlestick data
+    candles = info.candles_snapshot(name=symbol, interval=interval, startTime=start_time, endTime=end_time)
+    print(len(candles))
+    # end_time = start_time
 
 # Process the data as needed
-print(json.dumps(candles, indent=2))
+print(len(candles))
+# print(json.dumps(candles, indent=2))
